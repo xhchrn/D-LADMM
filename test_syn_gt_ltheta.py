44856b20f66c134ff74cfc4250f94fa8538e8abb
Xiaohan Chen <chernxh@tamu.edu>
2019-12-29 06:36:54 +0800

    Added more logging options

--- test_syn_gt_ltheta.py
+++ test_syn_gt_ltheta.py
@@ -406,8 +406,8 @@ elif objective == 'L1L1':
 
 elif objective == 'S-L2':
     # S_0 = self.S(self.Z0, self.E0, self.L0, T[-1], X, self.E0)
-    Ep = model.E0 if jj == 0 else E[-1]
-    Sjj = model.S(Z[jj], E[jj], L[jj], T[-1], Ep)
+    # Ep = model.E0 if jj == 0 else E[-1]
+    # Sjj = model.S(Z[jj], E[jj], L[jj], T[-1], Ep)
     sl2_values[jj] = sl2_values[jj] + model.two_norm(sl2_values).mean()
     print('L2 norms of S(uk):')
     for k in range(layers):
