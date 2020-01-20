0e3de22b80309ee11e643730c59d9eea8c6cd97b
Xiaohan Chen <xhchrn@gmail.com>
2020-01-20 13:49:26 +0800

    Continue L(S)KM with KM

--- test_syn_l1l1_scalar.py
+++ test_syn_l1l1_scalar.py
@@ -56,9 +56,10 @@ if not os.path.isdir('test-logs'):
     os.makedirs('test-logs')
 log_file = os.path.join(
     'test-logs',
-    'scalar-{obj}-results-{alg}-p{p}-mu{mu}-sigma{sigma}-{delta}{method}{param}.txt'.format(
+    'scalar-{obj}-results-{alg}{continued}-p{p}-mu{mu}-sigma{sigma}-{delta}{method}{param}.txt'.format(
         obj = objective.lower(),
         alg = 'lskm' if use_learned else 'km{}'.format(num_iter),
+        continued = '-continued{}'.format(num_iter) if continued else '',
         p = args.p, mu=args.mu, sigma = args.sigma,
         delta = 'delta{}-'.format(delta) if use_learned and use_safeguard else '',
         method = mu_k_method,
